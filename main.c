efbe9b33af9de720d40c3988eb0d5e86f708b445
user <user@user.myguest.virtualbox.org>
2024-03-28 13:43:22 +0700

    Calculator ver 1

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <arithmetic_operations.h>
+
+int main()
+{   
+    char choice;
+    float a, b;
+
+
+    
+    while(1)
+    {  
+         
+        printf("Choose arithmetic operation | + - * / |\n Or q to quit\n");
+        scanf("%c", &choice);
+
+        if(choice == 'q')
+        {
+            printf("You have quit the program");
+            return;
+        }
+
+        printf("Enter two numbers: ");
+        scanf("%f %f", &a, &b);
+        switch (choice)
+        {
+        case '+':
+            printf("Result: %f", add(a,b));
+            break;
+        case '-':
+            printf("Result: %f", subtract(a, b));
+            break;
+        case '*':
+            printf("Result: %f", multiply(a, b));
+            break;
+        case '/':
+            printf("Result: %f", divide(a, b));
+            break;
+        default:
+            printf("Invalid choice. Please, enter | + - * / | to choose arithmetic operation or q to quit\n");
+        }
+    }
+    return 0;
+}
